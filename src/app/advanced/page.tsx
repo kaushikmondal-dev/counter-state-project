ee0e508e44212da101f2f0790e8eaa2aa47bcd5e
Kaushik Mondal <kaushikmondal.dev@gmail.com>
2025-12-19 18:54:21 +0530

    advanced counter with logic done

--- src/app/advanced/page.tsx
+++ src/app/advanced/page.tsx
@@ -1,5 +1,26 @@
+import AdvancedCounter from "@/components/AdvancedCounter";
+import { Card, CardHeader, CardTitle } from "@/components/shadcnui/card";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Advanced | Counter State Project",
+	description: "Advanced page of Counter State Project",
+};
+
 const page = () => {
-	return <div>Enter</div>;
+	return (
+		<section className="grid h-[90dvh] place-items-center">
+			<Card className="w-xs">
+				<CardHeader>
+					<CardTitle className="text-center font-black">
+						Advanced Counter App
+					</CardTitle>
+
+					<AdvancedCounter />
+				</CardHeader>
+			</Card>
+		</section>
+	);
 };
 
 export default page;
---
+++ src/components/AdvancedCounter.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { CircleMinusIcon, CirclePlusIcon } from "lucide-react";
+import { useState } from "react";
+import { Button } from "./shadcnui/button";
+import { CardContent } from "./shadcnui/card";
+
+const AdvancedCounter = () => {
+	const [count, setCount] = useState(10);
+
+	const plus = () => {
+		//count = count + 1;
+		setCount(count + 1);
+	};
+
+	const minus = () => {
+		//count = count - 1;
+		setCount(count - 1);
+	};
+	const plusTen = () => {
+		//count = count + 1;
+		setCount(count + 10);
+	};
+
+	const minusTen = () => {
+		//count = count - 1;
+		setCount(count - 10);
+	};
+
+	return (
+		<CardContent className="text-center">
+			<div>Counter Value {count}</div>
+			<div className="grid grid-cols-2 gap-4 pt-4">
+				<Button
+					onClick={minus}
+					className="cursor-pointer px-4"
+					variant={"destructive"}>
+					<CircleMinusIcon />
+					Minus 1
+				</Button>
+				<Button
+					onClick={plus}
+					className="cursor-pointer px-4"
+					variant={"default"}>
+					<CirclePlusIcon />
+					Plus 1
+				</Button>
+				<Button
+					onClick={minusTen}
+					className="cursor-pointer px-4"
+					variant={"destructive"}>
+					<CircleMinusIcon />
+					Minus 10
+				</Button>
+				<Button
+					onClick={plusTen}
+					className="cursor-pointer px-4"
+					variant={"default"}>
+					<CirclePlusIcon />
+					Plus 10
+				</Button>
+			</div>
+		</CardContent>
+	);
+};
+
+export default AdvancedCounter;
